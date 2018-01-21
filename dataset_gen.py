5ad943e80aea3ec4df0277062f237106368a46fb
nikhilnj96 <nikhilnj96@gmail.com>
2018-01-21 19:49:16 +0530

    Update dataset_gen.py

--- dataset_gen.py
+++ dataset_gen.py
@@ -1,6 +1,6 @@
 import subprocess
-command = "./fb 42 8 & ./bb 81000 -8 & ./hp 400 7 & ./mmo 1725 -17 & wait"
 
+command = "./fib40 40 8 & ./bub5000 10000 -8 & ./hs500 500 7 & ./mat500 500 -17 & ./ms100000 100000 19 & wait"
 process = subprocess.Popen(command , stdout=subprocess.PIPE, shell = True)
 proc_stdout = process.communicate()[0].strip()
 print proc_stdout
