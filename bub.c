35813e629a813bff0f40aa5982c7ad61e55b1286
nikhilnj96 <nikhilnj96@gmail.com>
2018-01-26 19:54:59 +0530

    Add files via upload

---
+++ bub.c
@@ -0,0 +1,57 @@
+// Bubble sort - O(n^2)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+long long *a, INPUT_SIZE;
+int64_t start,end;
+
+void bub() {
+    long long i,j,temp;
+    bool swapped = false;
+
+    for(i = 0; i < INPUT_SIZE-1; i++) {
+        swapped = false;
+        for(j = 0; j < INPUT_SIZE-1-i; j++) {
+            if(a[j] > a[j+1]) {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped) {
+            break;
+        }
+    }
+}
+
+int64_t getTime() {
+    struct timespec tms;
+    if (clock_gettime(CLOCK_MONOTONIC,&tms))
+        return -1;
+    int64_t micros = tms.tv_sec * 1000000;
+    micros += tms.tv_nsec/1000;
+    if (tms.tv_nsec % 1000 >= 500)
+        ++micros;
+    return micros;
+}
+
+void main(int argc, char *argv[]) {
+    INPUT_SIZE = atoi(argv[1]);
+    nice(atoi(argv[2]));
+
+	a = malloc (INPUT_SIZE * sizeof(int));
+    for (long long i=0; i< INPUT_SIZE; i++)
+        a[i] = rand() % 1000000;
+
+    start = getTime();
+    bub();
+    end = getTime();
+
+    printf("bub,%"PRId64",%"PRId64",%"PRId64"\n",start,end,end - start);
+}
