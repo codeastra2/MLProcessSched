cbc3d2afc2d7235d51f95a332250faee2d31841d
nikhilnj96 <nikhilnj96@gmail.com>
2018-03-09 19:29:37 +0530

    Add new code for sort and time function

--- programs/hs.c
+++ programs/hs.c
@@ -1,73 +1,66 @@
 // Heap Sort - O(n log n)
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include <inttypes.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-#define INPUT_SIZE 10
-#define NICE_VALUE 0
+#define INPUT_SIZE 1000
+#define NICE_VALUE -18
 
 long long *a;
+struct timeval time;
 int64_t start,end;
 
-void hs() {
-    long long i, j, c, root, temp;
-    for (i = 1; i < INPUT_SIZE; i++) {
-        c = i;
-        do {
-            root = (c - 1) / 2;
-            if (a[root] < a[c]) {
-                temp = a[root];
-                a[root] = a[c];
-                a[c] = temp;
-            }
-            c = root;
-        } while (c != 0);
-    }
+void heapify(long long n, long long i) {
+    long long largest = i;
+    long long l = 2*i + 1;
+    long long r = 2*i + 2;
 
-    for (j = INPUT_SIZE - 1; j >= 0; j--) {
-        temp = a[0];
-        a[0] = a[j];
-        a[j] = temp;
-        root = 0;
-        do {
-            c = 2 * root + 1;
-            if ((a[c] < a[c + 1]) && c < j-1)
-                c++;
-            if (a[root]<a[c] && c<j)  {
-                temp = a[root];
-                a[root] = a[c];
-                a[c] = temp;
-            }
-            root = c;
-        } while (c < j);
+    if (l < n && a[l] > a[largest])
+        largest = l;
+
+    if (r < n && a[r] > a[largest])
+        largest = r;
+
+    if (largest != i) {
+        long long temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+        heapify(n, largest);
+    }
+}
+
+void hs(long long n) {
+    long long i;
+
+    for (i = n/2 - 1; i >= 0; i--)
+        heapify(n, i);
+
+    for (i = n - 1; i>=0; i--) {
+        long long temp = a[0];
+        a[0] = a[i];
+        a[i] = temp;
+        heapify(i, 0);
     }
 }
 
 int64_t getTime() {
-    struct timespec tms;
-    if (clock_gettime(CLOCK_MONOTONIC,&tms))
-        return -1;
-    int64_t micros = tms.tv_sec * 1000000;
-    micros += tms.tv_nsec/1000;
-    if (tms.tv_nsec % 1000 >= 500)
-        ++micros;
-    return micros;
+    gettimeofday(&time, NULL);
+    return time.tv_sec * 1000000 + time.tv_usec;
 }
 
 void main() {
     nice(NICE_VALUE);
 
-	a = malloc(INPUT_SIZE * sizeof(int));
+    a = malloc(INPUT_SIZE * sizeof(long long));
     for (long long i = 0; i < INPUT_SIZE; i++)
         a[i] = rand() % 1000000;
 
     start = getTime();
-    hs();
+    hs(INPUT_SIZE);
     end = getTime();
 
-    printf("hs,%"PRId64",%"PRId64",%"PRId64"\n",start,end,end - start);
+    printf("hs,%"PRId64",%"PRId64",%"PRId64"\n", start, end, end - start);
 }
