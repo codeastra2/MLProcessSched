9dd5bd17026846711a151267352f3978b6471f53
nikhilnj96 <nikhilnj96@gmail.com>
2018-03-09 19:27:27 +0530

    Add new code for time function

--- programs/bub.c
+++ programs/bub.c
@@ -1,53 +1,46 @@
 // Bubble sort - O(n^2)
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include <inttypes.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-#define INPUT_SIZE 10
-#define NICE_VALUE 0
+#define INPUT_SIZE 1000
+#define NICE_VALUE -18
 
 long long *a;
+struct timeval time;
 int64_t start,end;
 
 void bub() {
-    long long i,j,temp;
-    bool swapped = false;
+    long long i, j, temp;
+    short swapped;
 
     for(i = 0; i < INPUT_SIZE-1; i++) {
-        swapped = false;
+        swapped = 0;
         for(j = 0; j < INPUT_SIZE-1-i; j++) {
             if(a[j] > a[j+1]) {
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
-                swapped = true;
+                swapped = 1;
             }
         }
-        if(!swapped) {
-            break;
-        }
+        if(swapped == 0)
+            return;
     }
 }
 
 int64_t getTime() {
-    struct timespec tms;
-    if (clock_gettime(CLOCK_MONOTONIC,&tms))
-        return -1;
-    int64_t micros = tms.tv_sec * 1000000;
-    micros += tms.tv_nsec/1000;
-    if (tms.tv_nsec % 1000 >= 500)
-        ++micros;
-    return micros;
+    gettimeofday(&time, NULL);
+    return time.tv_sec * 1000000 + time.tv_usec;
 }
 
 void main() {
     nice(NICE_VALUE);
 
-	a = malloc (INPUT_SIZE * sizeof(int));
+    a = malloc (INPUT_SIZE * sizeof(long long));
     for (long long i=0; i< INPUT_SIZE; i++)
         a[i] = rand() % 1000000;
 
@@ -55,5 +48,5 @@ void main() {
     bub();
     end = getTime();
 
-    printf("bub,%"PRId64",%"PRId64",%"PRId64"\n",start,end,end - start);
+    printf("bub,%"PRId64",%"PRId64",%"PRId64"\n", start, end, end - start);
 }
