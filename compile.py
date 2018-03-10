04cef0ff52b9c15021bc95f66b64437d519f07f1
nikhilnj96 <nikhilnj96@gmail.com>
2018-03-10 21:50:01 +0530

    Add range to start and end program

--- compile.py
+++ compile.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE
-import os, csv
+import os, shutil, csv
 
 
 INPUT = os.getcwd() + '/programs/'
@@ -11,6 +11,7 @@ codes = {}
 1. Writes new .c files with next set of input sizes and nice values
 2. Compiles and move executables to separate folder for that set
 '''
+
 def write(values, i):
 	global INPUT, OUTPUT, codes
 
@@ -23,6 +24,7 @@ def write(values, i):
 		new_code = codes[code][:]
 		new_input_size = str(values[code][0])
 		new_nice_value = str(values[code][1])
+		''' Line numbers 9 and 10 in .c files '''
 		new_code[8] = '#define INPUT_SIZE ' + new_input_size + '\n'
 		new_code[9] = '#define NICE_VALUE ' + new_nice_value + '\n'
 		name = new_path + '/' + code.split('.')[0] + '_' + new_input_size + '_' + new_nice_value + '.' + code.split('.')[1]
@@ -38,6 +40,7 @@ def write(values, i):
 '''
 Reads input c programs only once
 '''
+
 def read_codes():
 	global INPUT, OUTPUT, codes
 
@@ -49,25 +52,37 @@ def read_codes():
 
 def main():
 	global INPUT, OUTPUT, codes
-
-	if not os.path.exists(OUTPUT):
-		os.makedirs(OUTPUT)
+	
+	''' Deletes existing exe folder, if any '''
+	if os.path.exists(OUTPUT):
+		shutil.rmtree(OUTPUT)
+	
+	os.makedirs(OUTPUT)
 
 	read_codes()
+	
+	''' Range to be entered by user. It corresponds to row id in input.csv '''
+	start, end = raw_input('Enter start and end index (both inclusive) to run\n').split(' ')
+	start = int(start)
+	end = int(end)
 
 	with open('input.csv', 'r') as csvfile:
 		i = 1
 		rows = csv.reader(csvfile)
+		
 		for row in rows:
-			print ('Compiling ' + row[0])
-			del row[0]
-			values = {}
-			values[row[0]] = [int(row[1]), int(row[2])]
-			values[row[3]] = [int(row[4]), int(row[5])]
-			values[row[6]] = [int(row[7]), int(row[8])]
-			values[row[9]] = [int(row[10]), int(row[11])]
-			values[row[12]] = [int(row[13]), int(row[14])]
-			write(values, i)
+			''' Compiles programs within the inout range '''
+			if i >= start and i<=end:
+				print ('Compiling ' + row[0])
+				del row[0]
+				
+				values = {}
+				values[row[0]] = [int(row[1]), int(row[2])]
+				values[row[3]] = [int(row[4]), int(row[5])]
+				values[row[6]] = [int(row[7]), int(row[8])]
+				values[row[9]] = [int(row[10]), int(row[11])]
+				values[row[12]] = [int(row[13]), int(row[14])]
+				write(values, i)
 			i += 1
 
 
