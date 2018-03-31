4d6d109443e8680d3b2600cd3b518676cb4b1a51
nikhilnj96 <nikhilnj96@gmail.com>
2018-03-31 14:05:28 +0530

    Add files via upload

---
+++ individual_programs/compile.py
@@ -0,0 +1,117 @@
+from subprocess import Popen, PIPE
+from datetime import datetime
+import os
+import shutil
+import csv
+import itertools
+import random
+import re
+
+
+INPUT = os.getcwd() + '/programs/'
+OUTPUT = os.getcwd() + '/exe/'
+ERROR = os.getcwd() + '/error/'
+codes = {}
+nice_values = [-18, -9, 0, 9, 18]
+
+
+'''
+1. Writes new .c files with input size and nice value
+2. Compiles and moves executables to separate folder
+'''
+
+def write(program_name, program_code, input_size, nice_value):
+	global INPUT, OUTPUT
+
+	new_path = (OUTPUT + program_name)
+	if not os.path.exists(new_path):
+		os.makedirs(new_path)
+
+	try:
+		new_code = program_code[:]
+		
+		''' Line numbers 9 and 10 in .c files '''
+		new_code[8] = '#define INPUT_SIZE ' + str(input_size) + '\n'
+		new_code[9] = '#define NICE_VALUE ' + str(nice_value) + '\n'
+
+		allocation_method = random.randint(0,2)
+		allocation_lines_1 = map(int, new_code[11].split('METHOD_1 ')[1].split(','))
+		allocation_lines_2 = map(int, new_code[12].split('METHOD_2 ')[1].split(','))
+		allocation_lines_3 = map(int, new_code[13].split('METHOD_3 ')[1].split(','))
+
+		new_code[allocation_lines_1[0]-1], new_code[allocation_lines_1[1]-1] = '/*\n', '*/\n'
+		new_code[allocation_lines_2[0]-1], new_code[allocation_lines_2[1]-1] = '/*\n', '*/\n'
+		new_code[allocation_lines_3[0]-1], new_code[allocation_lines_3[1]-1] = '/*\n', '*/\n'
+		new_code[allocation_lines_3[2]-1], new_code[allocation_lines_3[3]-1] = '/*\n', '*/\n'
+
+		if allocation_method == 0:
+			# Static variable
+			new_code[allocation_lines_1[0]-1], new_code[allocation_lines_1[1]-1] = '\n', '\n'
+		elif allocation_method == 1:
+			# Non-static variable
+			new_code[allocation_lines_2[0]-1], new_code[allocation_lines_2[1]-1] = '\n', '\n'
+		else:
+			# Pointer - Dynamic
+			new_code[allocation_lines_3[0]-1], new_code[allocation_lines_3[1]-1] = '\n', '\n'
+			new_code[allocation_lines_3[2]-1], new_code[allocation_lines_3[3]-1] = '\n', '\n'
+
+		'''
+		for _ in new_code:
+			print _,
+		raw_input()
+		'''
+
+		with open(INPUT + program_name + '.c', 'w') as w:
+			w.writelines(new_code)
+		
+		''' 2 '''
+		command = 'gcc %s -o %s/%s_%s_%s' % (INPUT + program_name + '.c', new_path, program_name.split('.')[0], input_size, nice_value)
+		#print command
+		
+		if Popen(command, stdout=PIPE, stderr = PIPE, shell = True).communicate()[1] != '':
+			raise Exception('Error in gcc')			
+
+	except Exception as e:
+		print(e)
+		shutil.rmtree(new_path)
+		with open(ERROR + 'error.txt', 'a') as f:
+			f.write(str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + '\t' + 'Compiling\tNew\t' + str(e) + '\n')
+
+
+'''
+Reads input c programs only once
+'''
+
+def read_codes():
+	global INPUT, OUTPUT, codes
+
+	for file in os.listdir(INPUT):
+		pointer = open(INPUT + file)
+		codes[file] = pointer.readlines()
+		pointer.close()
+
+
+def main():
+	global INPUT, OUTPUT, codes
+	
+	''' Deletes existing exe folder, if any '''
+	if os.path.exists(OUTPUT):
+		shutil.rmtree(OUTPUT)
+	os.makedirs(OUTPUT)
+
+	if not os.path.exists(ERROR):
+		os.makedirs(ERROR)
+
+	read_codes()
+	
+	with open('input.csv', 'r') as csvfile:
+		rows = csv.reader(csvfile)
+
+		for row in rows:
+			''' Compile for each combination '''
+			print('Compiling ' + row[0].split('.')[0] + '_' + str(row[1]) + '_' + str(row[2]))
+			write(row[0].split('.')[0], codes[row[0]], row[1], row[2])
+
+
+if __name__ == "__main__":
+	main()
---
+++ individual_programs/run.py
@@ -0,0 +1,136 @@
+from subprocess import Popen, PIPE
+from os import listdir
+from datetime import datetime
+import os.path, csv
+
+
+INPUT = os.getcwd()+ '/exe/'
+OUTPUT = os.getcwd() + '/csv/'
+ERROR = os.getcwd() + '/error/'
+attributes = []
+data = {}
+date_time = str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+
+
+'''
+5 attributes
+'''
+def command_size(command, input_size, nice_value):
+	global INPUT, attributes, data
+
+	s = Popen(command, shell=True, stdout=PIPE).stdout
+	lines = list(s)
+	data['.text'] = int(lines[1].split()[0])
+	data['.data'] = int(lines[1].split()[1])
+	data['.bss'] = int(lines[1].split()[2])
+	data['.input_size'] = int(input_size)
+	data['.nice_value'] = int(nice_value)
+
+
+'''
+27 attributes
+'''
+def command_readelf(command):
+	global INPUT, attributes, data
+
+	s = Popen(command, shell=True, stdout=PIPE).stdout
+	lines = list(s)
+	count = int(lines[0].split(' ')[2])
+	required_keys = ['.interp','.note.ABI-tag','.note.gnu.build-id', '.gnu.hash', '.dynsym', '.dynstr', '.gnu.version', '.gnu.version_r', '.rela.dyn', '.rela.plt', '.init', '.plt', '.plt.got','.fini', '.rodata', '.eh_frame_hdr', '.eh_frame', '.init_array', '.fini_array', '.jcr', '.dynamic', '.got', '.got.plt', '.comment', '.shstrtab', '.symtab', '.strtab']
+	
+	for i in range(1, count):
+		line = lines[4+i].split()
+		if i <= 9:
+			if line[2] in required_keys:
+				data[line[2]] = int(line[6], 16)
+		else:
+			if line[1] in required_keys:
+				data[line[1]] = int(line[5], 16)
+
+
+'''
+Creates a line of text with keys in sorted order
+'''
+def create_csv_row(file):
+	global INPUT, attributes, data
+	li = []
+	keys = data.keys()
+	keys.sort()
+	if not attributes:
+		attributes = keys[:]
+		attributes += ['start_time', 'end_time', 'total_time']
+	
+	for key in keys:
+		li.append(data[key])
+	li = [file.split('_')[0]] + li
+	return li
+
+
+'''
+1. Collects attributes from readelf
+2. Collects attributes from size
+3. Runs the executables
+4. Collects attributes post execution
+5. Log to CSV
+'''
+def run_set(path):
+	global INPUT, attributes, data
+
+	run = ''
+	output_1 = []
+	output_2 = []
+	
+	''' 1 and 2'''
+	for file in os.listdir(path):
+		print file
+		data = {}
+		run = './' + file + ' & '
+		command_readelf('readelf -SW %s' % path + file)
+		command_size('size %s' % path + file, file.split('_')[1], file.split('_')[2])
+		output_1 = create_csv_row(file)
+
+		''' 3 '''
+		run += 'wait'
+		output_2 = Popen(run, stdout=PIPE, shell = True, cwd = path).communicate()[0].strip()
+
+		''' 4 '''
+		output_2 = output_2.split(',')[1:]
+		output_2 = [int(x) for x in output_2]
+
+		''' 5 '''
+		with open(OUTPUT + date_time + '.csv', 'a') as f:
+			w = csv.writer(f, dialect='excel')
+			if os.stat(OUTPUT + date_time + '.csv').st_size == 0:
+				w.writerow(['id']+attributes)
+			if len(output_1 + output_2) != 34:
+				raise Exception('Less attributes')
+			else:
+				w.writerow(output_1 + output_2)
+
+
+def main():
+	global INPUT, attributes, data
+	
+	if not os.path.exists(OUTPUT):
+		os.makedirs(OUTPUT)
+
+	if not os.path.exists(ERROR):
+		os.makedirs(ERROR)
+
+	directories = os.listdir(INPUT)
+	directories.sort()
+	
+	for directory in directories:
+		
+		try:
+			print('Running ' + str(directory))
+			run_set(INPUT + str(directory) + '/')
+
+		except Exception as e:
+			print(e)
+			with open(ERROR + 'error.txt', 'a') as f:
+				f.write(date_time + '\t' + 'Running New\t' + str(directory) + '\t' + str(e) + '\n')
+
+
+if __name__ == "__main__":
+	main()
