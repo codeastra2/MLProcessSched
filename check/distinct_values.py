3c1f7992bc822a29ccfb67208c93889a96afa30e
nikhilnj96 <nikhilnj96@gmail.com>
2018-03-31 17:06:45 +0530

    Update distinct_values.py

--- check/distinct_values.py
+++ check/distinct_values.py
@@ -1,4 +1,8 @@
-import csv, os
+import csv
+import os
+
+INPUT_BATCH = os.getcwd().rpartition('/')[0] + '/batch_programs/csv'
+INPUT_INDIVIDUAL = os.getcwd().rpartition('/')[0] + '/individual_programs/csv'
 
 m = {}
 keys=['id', '.bss', '.comment', '.data', '.dynamic', '.dynstr', '.dynsym', '.eh_frame', '.eh_frame_hdr', '.fini', '.fini_array', '.gnu.hash', '.gnu.version', '.gnu.version_r', '.got', '.init', '.init_array', '.input_size', '.interp', '.nice_value', '.note.ABI-tag', '.note.gnu.build-id', '.plt', '.plt.got', '.rela.dyn', '.rela.plt', '.rodata', '.shstrtab', '.strtab', '.symtab', '.text', 'start_time', 'end_time', 'total_time']
@@ -14,9 +18,12 @@ for key in keys:
 	for program in programs:
 		m[program][key]=[]
 
-for file in os.listdir(os.getcwd()):
+choice = int(raw_input('Enter 1 for batch, 2 for individual\n'))
+choice = INPUT_BATCH if choice == 1 else INPUT_INDIVIDUAL
+
+for file in os.listdir(choice):
 	if len(file.split('.')) >1 and file.split('.')[1] == 'csv':
-		f=open(os.getcwd()+'/'+file)
+		f=open(choice+'/'+file)
 		for line in f.readlines():
 			line=line.split(',')
 			if line[0] in programs:
@@ -27,8 +34,7 @@ for file in os.listdir(os.getcwd()):
 
 print(count)
 
-#Edit here to direct to the output file to check
-with open(os.getcwd()+'/'+'output.csv', 'w') as f:
+with open(os.getcwd() + '/' + 'output_' + choice.split('_')[0].rpartition('/')[2] + '.csv', 'w') as f:
 	writer = csv.writer(f)
 	writer.writerow(['program'] + programs)
 	for key in keys[1:]:
