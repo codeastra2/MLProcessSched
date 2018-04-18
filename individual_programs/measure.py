db7a33019af17273a3c3e7dcc9b0b49d9649d0cd
nikhilnj96 <nikhilnj96@gmail.com>
2018-04-18 14:54:46 +0530

    Update measure.py

--- individual_programs/measure.py
+++ individual_programs/measure.py
@@ -5,6 +5,7 @@ import pandas
 import csv
 import os
 import matplotlib.pyplot as plt
+import matplotlib.widgets as mwidgets
  
 
 df = pandas.read_csv('measure_' + sys.argv[1] + '.csv')
@@ -51,14 +52,14 @@ def count():
 
 
 def write_results():
-	li1 = [sum(time_zero_nice_total), sum(time_custom_nice_total), sum(time_zero_nice_total) - sum(time_custom_nice_total), (sum(time_zero_nice_total) - sum(time_custom_nice_total))/len(time_zero_nice_total), (1.0 * sum(time_custom_nice_total)/sum(time_zero_nice_total))]
-	li2 = [sum(time_zero_nice_better), sum(time_custom_nice_better), sum(time_zero_nice_better) - sum(time_custom_nice_better), (sum(time_zero_nice_better) - sum(time_custom_nice_better))/len(time_zero_nice_better), (1.0 * sum(time_custom_nice_better)/sum(time_zero_nice_better))]
-	li3 = [sum(time_zero_nice_worse), sum(time_custom_nice_worse), sum(time_zero_nice_worse) - sum(time_custom_nice_worse), (sum(time_zero_nice_worse) - sum(time_custom_nice_worse))/len(time_zero_nice_worse), (1.0 * sum(time_custom_nice_worse)/sum(time_zero_nice_worse))]
+	li1 = [sum(time_zero_nice_total), sum(time_custom_nice_total), sum(time_zero_nice_total) - sum(time_custom_nice_total), (sum(time_zero_nice_total) - sum(time_custom_nice_total))/len(time_zero_nice_total), (better_ratio * 100), (1.0 * sum(time_custom_nice_total)/sum(time_zero_nice_total))]
+	li2 = [sum(time_zero_nice_better), sum(time_custom_nice_better), sum(time_zero_nice_better) - sum(time_custom_nice_better), (sum(time_zero_nice_better) - sum(time_custom_nice_better))/len(time_zero_nice_better), (better_ratio * 100), (1.0 * sum(time_custom_nice_better)/sum(time_zero_nice_better))]
+	li3 = [sum(time_zero_nice_worse), sum(time_custom_nice_worse), sum(time_zero_nice_worse) - sum(time_custom_nice_worse), (sum(time_zero_nice_worse) - sum(time_custom_nice_worse))/len(time_zero_nice_worse), (better_ratio * 100), (1.0 * sum(time_custom_nice_worse)/sum(time_zero_nice_worse))]
 
 	with open('results.csv', 'a') as f:
 		w = csv.writer(f, dialect='excel')
 		if os.stat('results.csv').st_size == 0:
-			w.writerow(['model','case', 'Time for 0 nice value', 'Time for custom nice value', 'Time saved', 'Time saved per program', 'Ratio of custom and 0 nice'])
+			w.writerow(['model','case', 'Time for 0 nice value', 'Time for custom nice value', 'Time saved', 'Time saved per program', 'Percentage of programs better','Ratio of custom and 0 nice'])
 		w.writerow([sys.argv[1], 'total'] + li1)
 		w.writerow([sys.argv[1], 'better'] + li2)
 		w.writerow([sys.argv[1], 'worse'] + li3)
@@ -76,12 +77,29 @@ def write_results():
 
 def plots():
 	len_list = [point for point in range(1, len(better_time)+1)]
-	plt.scatter(len_list, better_time, s=4)
 	 
-	plt.title('Scatter plot for showing how better out program is compared to default')
-	plt.xlabel('Succes ratio = %s percent)' % better_ratio * 100)
-	plt.ylabel('Better time (in microseconds)')
-	plt.show()
+	#plt.hist([max(a-b,0) for a,b in zip(time_zero_nice_total, time_custom_nice_total)], bins = 100, histtype = 'stepfilled', color='g')
+	#plt.bar(range(len(time_custom_nice_total)), [a-b for a,b in zip(time_zero_nice_total, time_custom_nice_total)], color='g')
+	plt.title('Scatter plot for showing time saved by custom nice value')
+	plt.xlabel('Testing data instances')
+	plt.ylabel('Time saved (in microseconds)')
+	#plt.show()
+	
+	plt.subplot(211)
+	plt.plot(range(len(time_custom_nice_total[0:100])), time_custom_nice_total[0:100])
+	plt.plot(range(len(time_zero_nice_total[0:100])), time_zero_nice_total[0:100])
+	plt.title('Turn around time comparison ')
+	plt.ylabel('Time (in microseconds)')
+	plt.legend(['Custom nice value', 'Zero nice value'], loc = 'upper left')
 
+	plt.subplot(212)
+	plt.plot(range(len(time_custom_nice_total[100:200])), time_custom_nice_total[100:200])
+	plt.plot(range(len(time_zero_nice_total[100:200])), time_zero_nice_total[100:200])
+	plt.xlabel('Testing data instances')
+	plt.ylabel('Time (in microseconds)')
+	plt.legend(['Custom nice value', 'Zero nice value'], loc = 'upper left')
+
+	plt.show()
+	
 
 count()
