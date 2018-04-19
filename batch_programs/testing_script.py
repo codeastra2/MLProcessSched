9ff4549f4f9f01527587da0e0fbd5db94d85a068
nikhilnj96 <nikhilnj96@gmail.com>
2018-04-19 20:26:44 +0530

    Update testing_script.py

--- batch_programs/testing_script.py
+++ batch_programs/testing_script.py
@@ -1,28 +1,29 @@
-import csv
-
 from sklearn.externals import joblib
-import pandas
 
-filename = "model_batch.sav"
+import sys
+import csv
+import pandas
+import random
+
+filename = 'model_' + sys.argv[1] +'.sav'
 model = joblib.load(filename)
+min_nice = -15
+max_nice = 19
 
 prog_rev = {"1":"bub.c", "2":"fib.c", "3":"mat.c", "4":"ms.c"}
-nice_values = [-15, -7, 0, 7, 15]
 nice_value_list = []
-for val1 in nice_values:
-    for val2 in nice_values:
-        for val3 in nice_values:
-            for val4 in nice_values:
-                nice_value_list.append([val1, val2, val3, val4])
+for i in range(50):
+    nice_value_list.append([random.randint(min_nice, max_nice), random.randint(min_nice, max_nice), random.randint(min_nice, max_nice), random.randint(min_nice, max_nice)])
 
-def find_best_nice_value(record):
+
+def find_best_nice_value(row):
     min_time = 9876543210
     best_nice_value = [0, 0, 0, 0]
-    record = record.reshape(1, -1)
+    row = row.reshape(1, -1)
     for nice_value in nice_value_list:
         for idx in range(0, 4):
-            record[0][19 + idx*31] = nice_value[idx]
-        exe_time = model.predict(record)[0]
+            row[0][19 + idx*31] = nice_value[idx]
+        exe_time = model.predict(row)[0]
         if exe_time < min_time:
             best_nice_value = nice_value
             min_time = exe_time
@@ -32,14 +33,16 @@ def find_best_nice_value(record):
 df = pandas.read_csv("proc_dataset.csv")
 array = df.values
 X = array[:, 0:124]
+
+
 new_input_list = []
-for record in X:
+for row in X:
     prog_names = []
     input_sizes = []
     for idx in range(0, 4):
-        prog_names.append(prog_rev[str(record[0+31*idx])])
-        input_sizes.append(record[17 + idx*31])
-    nice_values = find_best_nice_value(record)
+        prog_names.append(prog_rev[str(row[0+31*idx])])
+        input_sizes.append(row[17 + idx*31])
+    nice_values = find_best_nice_value(row)
     new_input = [len(new_input_list) + 1]
     for idx in range(0, 4):
         new_input.append(prog_names[idx])
@@ -53,10 +56,8 @@ for record in X:
         new_input.append(0)
     new_input_list.append(new_input)
 
-#print(new_input_list)
+
 with open('input.csv', 'w') as f:
     w = csv.writer(f, dialect='excel')
-    for input in new_input_list:
-        w.writerow(input)
-
-
+    for new_input in new_input_list:
+        w.writerow(new_input)
