4c249a80b1836c2b3f6089d9451104ddd16134e5
nikhilnj96 <nikhilnj96@gmail.com>
2018-05-01 20:47:27 +0530

    Update training_script.py

--- batch_programs/training_script.py
+++ batch_programs/training_script.py
@@ -9,7 +9,6 @@ from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
-from sklearn.neural_network import MLPClassifier, MLPRegressor
 
 from sklearn.metrics import r2_score
 from sklearn.metrics import explained_variance_score
@@ -26,63 +25,65 @@ import scikitplot
 import matplotlib.pyplot as plt
 
 
-def process_op_line(line):
-    op_list = line.split(',')
-    op_list[0] = prog_map[op_list[0]]
-    del op_list[32]
-    del op_list[31]
-    new_line = ",".join(op_list)
-    new_line=new_line.strip()
-    return new_line
+'''
+Extract, Transform, Load
+'''
+def etl():
+    lines = []
+    new_lines = []
+    header = "*"+ ",*"*124
+    
+    with open('dataset.csv', 'r') as f:
+        lines = [line for line in f]
 
-
-def combine_lines(lines):
-    net_time = 0
-    comb_line = ""
-    for line in lines:
+    # Removes start time and end time from a line
+    def strip_start_end_time(line):
+        prog_map = {"bub":"1", "fib":"2", "mat":"3", "ms":"4"}
         op_list = line.split(',')
-        net_time += int(op_list[-1])
-        del op_list[-1]
-        comb_line += ",".join(op_list)
-        comb_line += ","
-    comb_line += str(net_time)
-    return comb_line
+        op_list[0] = prog_map[op_list[0]]
+        del op_list[32]
+        del op_list[31]
+        new_line = ",".join(op_list)
+        new_line=new_line.strip()
+        return new_line
 
+    # Combines 4 lines to 1 line
+    def combine_lines(lines):
+        net_time = 0
+        comb_line = ""
+        for line in lines:
+            op_list = line.split(',')
+            net_time += int(op_list[-1])
+            del op_list[-1]
+            comb_line += ",".join(op_list)
+            comb_line += ","
+        comb_line += str(net_time)
+        return comb_line
 
-lines = []
-with open('dataset.csv', 'r') as f:
-    lines = [line for line in f]
+    for index in range(1, len(lines), 4):
+        new_line = []
+        for idx in range(index, index+4):
+           new_line.append(strip_start_end_time(lines[idx]))
+        new_line = combine_lines(new_line)
+        new_lines.append(new_line)
 
+    # Isolate certain records for testing model at a later stage
+    isolate = int(0.8 * len(new_lines))
+    random.shuffle(new_lines)
+    dataset_isolated = [header] + new_lines[isolate:]
+    new_lines = [header] + new_lines[:isolate]
 
-new_lines = []
-programs = ["bub", "fib", "mat", "ms"]
-prog_output_dict = {"bub": "", "fib": "", "mat": "", "ms": ""}
-prog_map = {"bub":"1", "fib":"2", "mat":"3", "ms":"4"}
-line = "*"+ ",*"*124
-# new_lines.append(line)
-for index in range(1, len(lines), 4):
-    new_line = []
-    for idx in range(index, index+4):
-       new_line.append(process_op_line(lines[idx]))
+    with open('proc_dataset_isolated.csv', 'w') as f:
+        csvwriter = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for line in dataset_isolated:
+            csvwriter.writerow(line.split())
 
-    new_line = combine_lines(new_line)
-    new_lines.append(new_line)
+    with open('proc_dataset.csv', 'w') as f:
+        csvwriter = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for line in new_lines:
+            csvwriter.writerow(line.split())
 
-# Isolate certain records for testing model at a later stage
-random.shuffle(new_lines)
-dataset_isolated = [line] + new_lines[int(0.8 * len(new_lines)):]
-new_lines = [line] + new_lines[:int(0.8*len(new_lines))]
-
-with open('proc_dataset_isolated.csv', 'w') as f:
-    csvwriter = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for line in dataset_isolated:
-        csvwriter.writerow(line.split())
-
-with open('proc_dataset.csv', 'w') as f:
-    csvwriter = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for line in new_lines:
-        csvwriter.writerow(line.split())
-
+etl()
 Y_test = []
 prediction = []
 
@@ -91,13 +92,11 @@ array = df.values
 columns = df.columns
 X = array[:, 0:124]
 Y = array[:, 124]
-selected_columns = columns
 
 
 '''
 Decision Tree - C4.5
 '''
-
 def dt():
     global Y_test, prediction
     
@@ -110,7 +109,7 @@ def dt():
     prediction = model.predict(X_test)
     print_accuracy(model, 'model_dt')
 
-    scikitplot.estimators.plot_feature_importances(model, feature_names=selected_columns, x_tick_rotation = 90, order = None, text_fontsize='small')
+    scikitplot.estimators.plot_feature_importances(model, feature_names=columns, x_tick_rotation = 90, order = None, text_fontsize='small')
     plt.title('')
     plt.ylabel('Feature Importance')
     plt.show()
@@ -121,7 +120,6 @@ def dt():
 '''
 Decision Tree with ADA Boost
 '''
-
 def dtb():
     global Y_test, prediction
 
@@ -146,7 +144,6 @@ def dtb():
 '''
 Random Forest
 '''
-
 def rf():
     global Y_test, prediction
 
@@ -167,7 +164,6 @@ def rf():
 '''
 k Nearest Neighbour
 '''
-
 def knn():
     global Y_test, prediction
 
@@ -188,60 +184,6 @@ def knn():
     plt.xlabel('Testing data')
     plt.ylabel('Time in microseconds')
     plt.show()
-    
-
-'''
-Support Vector Machine
-'''
-
-def svm():
-    global Y_test, prediction
-    
-    test_size = 0.20
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=1)
-    
-    # This scales to negative values also. Hence some values in print_accuracy() may not be right.
-    X_train =  preprocessing.scale(X_train)
-    X_test = preprocessing.scale(X_test)
-    Y_train = preprocessing.scale(Y_train)
-    Y_test = preprocessing.scale(Y_test)
-
-    parameters = {'kernel':('linear', 'poly', 'rbf', 'sigmoid'), 'C':[1, 10], 'shrinking': (True, False)}
-    
-    #model = svm.SVR(C=1000, epsilon=0.1, kernel = 'rbf')
-    model = SVR()
-    model = GridSearchCV(model, parameters, verbose = 1)
-    model.fit(X_train, Y_train)
-    
-    prediction = model.predict(X_test)
-    print_accuracy(model, 'model_svm')
-
-
-'''
-Multi Layer Perceptron
-'''
-
-def mlp():
-    global Y_test, prediction
-    
-    test_size = 0.30
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=1)
-    
-    # This scales to negative values also. Hence some values in print_accuracy() may not be right.
-    X_train =  preprocessing.scale(X_train)
-    X_test = preprocessing.scale(X_test)
-    Y_train = preprocessing.scale(Y_train)
-    Y_test = preprocessing.scale(Y_test)
-    
-    param_grid = [{'activation' : ['identity', 'logistic', 'tanh', 'relu'], 'solver' : ['lbfgs', 'sgd', 'adam'], 'hidden_layer_sizes': [(1,),(5,),(10,),(15,),(20,),(30,),(50,)]}]
-    
-    model = MLPRegressor()
-    model = GridSearchCV(model, param_grid, verbose = 1)
-    model.fit(X_train, Y_train)
-
-    prediction = model.predict(X_test)
-    print_accuracy(model, 'model_mlp')
-
 
 
 def print_accuracy(model, name):
@@ -255,7 +197,7 @@ def print_accuracy(model, name):
     print('MAE = ' + str(mean_absolute_error(Y_test, prediction)))
     print('MSE = ' + str(mean_squared_error(Y_test, prediction)))
     joblib.dump(model, name + '.sav')
-    
+
 
 try:
     def undefined():
@@ -266,8 +208,6 @@ try:
         'dtb': dtb,
         'rf' : rf,
         'knn': knn,
-        'svm': svm,
-        'mlp': mlp,
     }.get(sys.argv[1], undefined)()
 
 except IndexError:
